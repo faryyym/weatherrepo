29ccc45607059d38001b76734aa8361a54dc6ad3
Ramy Ramone <ramy.remy@gmail.com>
2021-02-10 20:36:01 +0500

    basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherwidgetComponent } from './weatherwidget/weatherwidget.component';
+import { WeatherComponent } from './weather/weather.component';
+import { TwitterComponent } from './twitter/twitter.component';
+import { ForecastComponent } from './forecast/forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherwidgetComponent,
+    WeatherComponent,
+    TwitterComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/weatherwidget/weatherwidget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weatherwidget',
+  templateUrl: './weatherwidget.component.html',
+  styleUrls: ['./weatherwidget.component.scss']
+})
+export class WeatherwidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
