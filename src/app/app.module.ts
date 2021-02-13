3bde9af0c8f723477b71c914f8a88c4e7c59c267
Ramy Ramone <ramy.remy@gmail.com>
2021-02-13 19:42:57 +0500

    search done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { DatePipe } from '@angular/common';
-
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../weather.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-weather',
@@ -8,19 +9,30 @@ import { WeatherService } from '../weather.service';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements OnInit {
+  public weatherSearchForm: FormGroup;
   forecastWeather: any[] = [];
   curWeather: any;
 
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.getWeather()
+    this.weatherSearchForm = this.fb.group({
+      location: ['']
+    });
 
-    this.getWeatherForecast()
   }
 
-  getWeatherForecast() {
-    this.weatherService.getWeatherForecastService('India').subscribe(data => {
+  getWeather(formValue) {
+    this.getWeatherForecast(formValue.location)
+    this.weatherService.getWeatherService(formValue.location).subscribe(data => {
+      this.curWeather = data;
+      console.log(this.curWeather);
+    })
+  }
+
+  getWeatherForecast(cityName) {
+    this.weatherService.getWeatherForecastService(cityName).subscribe(data => {
 
 
       for (let i = 0; i < data.list.length; i++) {
@@ -32,12 +44,8 @@ export class WeatherComponent implements OnInit {
     })
   }
 
-  getWeather() {
-    this.weatherService.getWeatherService('India').subscribe(data => {
-      this.curWeather = data;
-      console.log(this.curWeather);
-    })
-  }
+
+
 
 
 
